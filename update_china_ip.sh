7736fc8035192203202a815190646fc6403b0ee7
LisonFan <lisonfan1996@gmail.com>
2017-06-22 04:46:45 +0000

    2017-06-22

---
+++ update_china_ip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+wget -O apnic https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest
+wget -O ipip https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
+
+# APNIC
+cat apnic | grep ipv4 | grep CN | awk -F\| '{printf("%s/%d\n", $4, 32-log($5)/log(2))}' >> temp_apnic_china_ip
+echo -e "\n" >> temp_apnic_china_ip
+
+# IPIP
+echo -e "\n" >> ipip
+
+# 合并 & 去重
+cat temp_apnic_china_ip ipip  | sort | uniq > temp_china_ip_list
+
+# 去空行
+grep -v '^$' temp_china_ip_list > temp_2_china_ip_list
+
+# 排序
+sort  -t "." -k1n,1 -k2n,2 -k3n,3 -k4n,4 temp_2_china_ip_list > china_ip_list
+
+# 清理
+rm -rf apnic ipip temp_apnic_china_ip temp_china_ip_list temp_2_china_ip_list
+
+# 提交
+CurrentDate=`date +%Y-%m-%d`
+git add .
+git commit -m $CurrentDate
+git push
