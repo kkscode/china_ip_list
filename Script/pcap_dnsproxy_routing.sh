dba764f749dc4a8736e3e4d2fcadadf0d2854676
LisonFan <lisonfan1996@gmail.com>
2017-08-11 10:08:50 +0800

    2017-08-11-10-08-50

--- Script/pcap_dnsproxy_routing.sh
+++ Script/pcap_dnsproxy_routing.sh
@@ -17,9 +17,10 @@ echo "\n" >> Pcap_DNSProxy_Routing.txt
 echo "## IPv6" >> Pcap_DNSProxy_Routing.txt
 cat /root/china_ip_list/apnic | grep ipv6 | grep CN | awk -F\| '{printf("%s/%d\n", $4, $5)}' >> Pcap_DNSProxy_Routing.txt
 
-# 清理
-rm -rf apnic ipip temp_apnic_china_ip temp_china_ip_list temp_2_china_ip_list
-
 mv Pcap_DNSProxy_Routing.txt Routing.txt
+
+# 清理
+cd /root/china_ip_list/
+rm -rf apnic ipip temp_apnic_china_ip temp_china_ip_list temp_2_china_ip_list
 
 /root/china_ip_list/Script/china_ip_for_surge_rules.sh
